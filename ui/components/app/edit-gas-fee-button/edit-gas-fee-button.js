4b20128c2fd35ff38ddc6635e3f39a6182c083bd
jpuri <jyotipuri@gmail.com>
2021-12-02 07:20:26 +0530

    Implementing review feedback

--- ui/components/app/edit-gas-fee-button/edit-gas-fee-button.js
+++ ui/components/app/edit-gas-fee-button/edit-gas-fee-button.js
@@ -21,54 +21,51 @@ export default function EditGasFeeButton({ userAcknowledgedGasMissing }) {
     transaction,
   } = useGasFeeContext();
   const { openModal } = useTransactionModalContext();
+  const editEnabled =
+    !hasSimulationError || userAcknowledgedGasMissing === true;
 
-  if (supportsEIP1559V2 && estimateUsed) {
-    const editEnabled =
-      !hasSimulationError || userAcknowledgedGasMissing === true;
-    if (!editEnabled) return null;
-
-    return (
-      <div className="edit-gas-fee-button">
-        <button onClick={() => openModal('editGasFee')}>
-          <span className="edit-gas-fee-button__icon">
-            {`${PRIORITY_LEVEL_ICON_MAP[estimateUsed]} `}
-          </span>
-          <span className="edit-gas-fee-button__label">{t(estimateUsed)}</span>
-          <i className="fas fa-chevron-right asset-list-item__chevron-right" />
-        </button>
-        {estimateUsed === 'custom' && (
-          <button onClick={() => openModal('advancedGasFee')}>
-            {t('edit')}
-          </button>
-        )}
-        {estimateUsed === 'dappSuggested' && (
-          <InfoTooltip
-            contentText={
-              <div className="edit-gas-fee-button__tooltip">
-                <Typography fontSize="12px" color={COLORS.GREY}>
-                  {t('dappSuggestedTooltip', [transaction.origin])}
-                </Typography>
-                <Typography fontSize="12px">
-                  <b>{t('maxBaseFee')}</b>
-                  {maxFeePerGas}
-                </Typography>
-                <Typography fontSize="12px">
-                  <b>{t('maxPriorityFee')}</b>
-                  {maxPriorityFeePerGas}
-                </Typography>
-                <Typography fontSize="12px">
-                  <b>{t('gasLimit')}</b>
-                  {gasLimit}
-                </Typography>
-              </div>
-            }
-            position="top"
-          />
-        )}
-      </div>
-    );
+  if (!supportsEIP1559V2 || !estimateUsed || !editEnabled) {
+    return null;
   }
-  return null;
+
+  return (
+    <div className="edit-gas-fee-button">
+      <button onClick={() => openModal('editGasFee')}>
+        <span className="edit-gas-fee-button__icon">
+          {`${PRIORITY_LEVEL_ICON_MAP[estimateUsed]} `}
+        </span>
+        <span className="edit-gas-fee-button__label">{t(estimateUsed)}</span>
+        <i className="fas fa-chevron-right asset-list-item__chevron-right" />
+      </button>
+      {estimateUsed === 'custom' && (
+        <button onClick={() => openModal('advancedGasFee')}>{t('edit')}</button>
+      )}
+      {estimateUsed === 'dappSuggested' && (
+        <InfoTooltip
+          contentText={
+            <div className="edit-gas-fee-button__tooltip">
+              <Typography fontSize="12px" color={COLORS.GREY}>
+                {t('dappSuggestedTooltip', [transaction.origin])}
+              </Typography>
+              <Typography fontSize="12px">
+                <b>{t('maxBaseFee')}</b>
+                {maxFeePerGas}
+              </Typography>
+              <Typography fontSize="12px">
+                <b>{t('maxPriorityFee')}</b>
+                {maxPriorityFeePerGas}
+              </Typography>
+              <Typography fontSize="12px">
+                <b>{t('gasLimit')}</b>
+                {gasLimit}
+              </Typography>
+            </div>
+          }
+          position="top"
+        />
+      )}
+    </div>
+  );
 }
 
 EditGasFeeButton.propTypes = {
